b7114c0ba025710befb5147615aeba3df3860e57
Ahmed Wagdy <a7med.wagdy96@gmail.com>
2022-03-22 16:04:10 +0200

    Update app.dart

--- app.dart
+++ app.dart
@@ -17,7 +17,7 @@ main() {
   dynamic dc = 3;
   print(dc);
   print(ob);
-  final arr = [1, 2, 3, 4];
+  final arr = [1, 2, 3];
   arr[0] = 0;
   const arr2 = [4, 5, 6, 7];
   // arr2[0] = 0; you can't change the const value or refrence
@@ -25,6 +25,25 @@ main() {
   print(arr2);
   bool isGood = false;
   print(isGood);
-  var list = [1, 2, 3, for (var i in arr2) i];
+  /* Dart 2.3 introduced 
+     spread operator …, null-aware spread operator  
+     collection if, collection for
+ */
+  var arrAll = [...arr, ...?arr2];
+  print(arrAll);
+  var list1 = [
+    1,
+    2,
+    3,
+    if (isGood) [8, 8] else [9, 10]
+  ];
+  print("list1 = $list1");
+  print("list[3] = ${list1[3]}");
+  print("list.length= ${list1.length}");
+  list1.add(11);
+  print("list.length= ${list1.length}");
+
+//collection for
+  var list = [1, 2, 3, for (var i in arr2) i + 5];
   print(list);
 }
